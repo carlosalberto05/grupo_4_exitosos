9ae90b6582f26b4b5ab8e3cf92255c6a21c4c01f
Carlos <carlos_alberto_lira@hotmail.com>
2021-12-17 13:20:08 -0600

    se anadio funcionalidad para editar y eliminar un disco

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 // ************ Require's ************
 const express = require("express");
+const logger = require("morgan");
 const path = require("path");
+const methodOverride = require("method-override"); // Pasar poder usar los métodos PUT y DELETE
 const port = process.env.PORT || 3000;
 
 // ************ express() - (don't touch) ************
@@ -9,7 +11,10 @@ const app = express();
 // ************ Middlewares - (don't touch) ************
 //Configuración para la carpeta public
 app.use(express.static(path.resolve(__dirname, "../public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(logger("dev"));
 app.use(express.json());
+app.use(methodOverride("_method")); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
 
 // ************ Template Engine - (don't touch) ************
 //Configuración para moto eje y la carpeta view
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -43,14 +43,58 @@ const productsController = {
 
     discs.push(newDisc);
 
-    console.log("Este es el newDisc");
-    console.log(newDisc);
-    console.log(req.body);
-
-    console.log(discs);
     fs.writeFileSync(discsFilePath, JSON.stringify(discs, null, " "));
 
+    // res.redirect("/");
+    return res.redirect("/products/detail/" + newDisc.id);
+  },
+
+  // Update - Form to edit
+  edit: (req, res) => {
+    let id = req.params.id;
+    let discToEdit = discs.find((disc) => disc.id == id);
+    res.render("products/product-edit-form.ejs", { discToEdit });
+  },
+  // Update - Method to update
+  update: (req, res) => {
+    let id = req.params.id;
+    let discToEdit = discs.find((disc) => disc.id == id);
+
+    console.log("El red.body");
+    console.log(req.body);
+
+    discToEdit = {
+      id: discToEdit.id,
+      ...req.body,
+      img: discToEdit.img,
+    };
+
+    discToEdit.tracklist = discToEdit.tracklist.split(",");
+
+    console.log("Disco que se va a editar");
+    console.log(discToEdit);
+
+    let newDiscs = discs.map((disc) => {
+      if (disc.id == discToEdit.id) {
+        return (disc = { ...discToEdit });
+      }
+      return disc;
+    });
+
+    console.log("Lista de discos incluyendo el editado");
+    console.log(newDiscs);
+
+    fs.writeFileSync(discsFilePath, JSON.stringify(newDiscs, null, " "));
     res.redirect("/");
+  },
+
+  // Delete - Delete one product from DB
+  destroy: (req, res) => {
+    let id = req.params.id;
+    let finalDiscs = discs.filter((disc) => disc.id != id);
+    fs.writeFileSync(discsFilePath, JSON.stringify(finalDiscs, null, " "));
+    discs = JSON.parse(fs.readFileSync(discsFilePath, "utf-8"));
+    res.redirect("/products");
   },
 };
 
