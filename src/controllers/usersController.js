23f8563965b3aebc159a12ba52ab11e675ebc52c
Carlos <carlos_alberto_lira@hotmail.com>
2021-12-22 12:27:33 -0600

    se realizaron actualizaciones

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -8,6 +8,7 @@ const usersController = {
   register: (req, res) => {
     return res.render("users/register");
   },
+
   processRegister: (req, res) => {
     let newUser = {
       id: users[users.length - 1].id + 1,
@@ -18,11 +19,13 @@ const usersController = {
     users.push(newUser);
     fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
 
-    return res.redirect("users/login");
+    return res.redirect("login");
   },
+
   login: (req, res) => {
     return res.render("users/login");
   },
+
   profile: (req, res) => {
     return res.render("users/userProfile");
   },
