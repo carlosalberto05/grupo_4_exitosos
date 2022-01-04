8f923f31a9ecced9c969d2136c3b539d398a40f4
Carlos <carlos_alberto_lira@hotmail.com>
2022-01-04 15:25:28 -0600

    se crearon middlewares para la autenticacion y se modificaron los controladores y rutas

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -8,11 +8,13 @@ const usersController = require("../controllers/usersController");
 //Middlewares
 const uploadFile = require("../middlewares/multerMiddleware");
 const validations = require("../middlewares/validateRegisterMiddleware");
+const guestMiddleware = require("../middlewares/guestMiddleware");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 //Estas son subrutas de la ruta /users que se configura en app
 
 //Formulario de registro
-router.get("/register", usersController.register);
+router.get("/register", guestMiddleware, usersController.register);
 
 //Procesar el registro
 router.post(
@@ -23,12 +25,15 @@ router.post(
 );
 
 //Formulario de login
-router.get("/login", usersController.login);
+router.get("/login", guestMiddleware, usersController.login);
 
 //Procesar el login
 router.post("/login", usersController.loginProcess);
 
 //Perfil del usuarior
-router.get("/profile", usersController.profile);
+router.get("/profile", authMiddleware, usersController.profile);
+
+//Logout
+router.get("/logout", usersController.logout);
 
 module.exports = router;
