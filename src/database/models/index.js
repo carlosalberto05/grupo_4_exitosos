3aaa93d7099a0bf714a06ed3e9865f143e6be63e
Carlos Alberto Lira <47789867+carlosalberto05@users.noreply.github.com>
2024-02-21 16:30:31 -0600

    nuevo cambio

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -12,7 +12,9 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(process.env.DATABASE_URL_PROD, config);
+  sequelize = new Sequelize(process.env.DATABASE_URL_PROD, {
+    dialect: "mysql",
+  });
 }
 
 fs.readdirSync(__dirname)
